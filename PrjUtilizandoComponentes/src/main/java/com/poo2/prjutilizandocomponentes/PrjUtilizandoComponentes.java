3480ba82108bc9b97a3f4bca2cdfe1fb112e4783
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-10-15 16:49:02 -0300

    Add files via upload

---
+++ PrjUtilizandoComponentes/src/main/java/com/poo2/prjutilizandocomponentes/PrjUtilizandoComponentes.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.poo2.prjutilizandocomponentes;
+
+import formularios.FormCalculadoraSegGrau;
+
+/**
+ *
+ * @author Lucas
+ */
+public class PrjUtilizandoComponentes {
+
+    public static void main(String[] args) {
+        FormCalculadoraSegGrau formCalcSeg= new FormCalculadoraSegGrau();
+        
+        formCalcSeg.setVisible(true);
+    }
+}
