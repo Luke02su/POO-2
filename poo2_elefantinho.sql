3f79e1e397b43d7b9d747f5c431ff70d1216f31a
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-10-22 20:43:31 -0300

    Add files via upload

---
+++ poo2_elefantinho.sql
@@ -0,0 +1,29 @@
+CREATE TABLE Pessoa (
+	idpessoa SERIAL PRIMARY KEY,
+    nome VARCHAR(35) NOT NULL,
+    data_nascimento DATE NOT NULL
+);
+
+CREATE TABLE Pet (
+    idpet SERIAL PRIMARY KEY,
+    nome VARCHAR(80) NOT NULL,
+    raca VARCHAR(50) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    cor VARCHAR(50) NOT NULL,
+    idpessoa INT NOT NULL,
+    
+    FOREIGN KEY (idpessoa) REFERENCES Pessoa(idpessoa) ON UPDATE RESTRICT ON DELETE RESTRICT
+);
+
+CREATE INDEX idpessoa_idx ON Pessoa (idpessoa);
+
+INSERT INTO pessoa VALUES (default, 'Lucas', '2004-08-16');
+INSERT INTO pet VALUES (default, 'Toto', 'Shitzu', '2020-09-18', 'Branco com preto', 1);
+
+SELECT * FROM pessoa;
+SELECT * FROM pet;
+
+SELECT * 
+FROM pessoa
+INNER JOIN pet
+ON pet.idpessoa = pessoa.idpessoa;
