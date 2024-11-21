970c0163756bca272d3ff2528a60402a47ac9239
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-11-21 20:00:33 -0300

    poo2_elefantinho.sql

--- poo2_elefantinho.sql
+++ poo2_elefantinho.sql
@@ -10,6 +10,7 @@ CREATE TABLE Pet (
     raca VARCHAR(50) NOT NULL,
     data_nascimento DATE NOT NULL,
     cor VARCHAR(50) NOT NULL,
+	porte VARCHAR(50) NOT NULL,
     idpessoa INT NOT NULL,
     
     CONSTRAINT fk_idpessoa FOREIGN KEY (idpessoa) REFERENCES Pessoa(idpessoa) ON UPDATE RESTRICT ON DELETE RESTRICT
@@ -18,7 +19,7 @@ CREATE TABLE Pet (
 CREATE INDEX idpessoa_idx ON Pessoa (idpessoa);
 
 INSERT INTO pessoa VALUES (default, 'Lucas', '2004-08-16');
-INSERT INTO pet VALUES (default, 'Toto', 'Shitzu', '2020-09-18', 'Branco com preto', 1);
+INSERT INTO pet VALUES (default, 'Toto', 'Shitzu', '2020-09-18', 'Branco com preto', 'Pequeno', 1);
 
 SELECT * FROM pessoa;
 SELECT * FROM pet;
