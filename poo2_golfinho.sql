fbeb7cc266ec588fab29b71bbbbe4d4aff912fca
Lucas <68867449+Luke02su@users.noreply.github.com>
2024-11-21 19:59:54 -0300

    Update poo2_golfinho.sql

--- poo2_golfinho.sql
+++ poo2_golfinho.sql
@@ -1,27 +1,25 @@
-CREATE SCHEMA bd_petshop;
-USE bd_petshop;
-
 CREATE TABLE Pessoa (
-	idpessoa INT AUTO_INCREMENT PRIMARY KEY,
+	idpessoa SERIAL PRIMARY KEY,
     nome VARCHAR(35) NOT NULL,
     data_nascimento DATE NOT NULL
 );
 
 CREATE TABLE Pet (
-	idpet INT AUTO_INCREMENT PRIMARY KEY,
+    idpet SERIAL PRIMARY KEY,
     nome VARCHAR(80) NOT NULL,
     raca VARCHAR(50) NOT NULL,
-	data_nascimento DATE NOT NULL,
+    data_nascimento DATE NOT NULL,
     cor VARCHAR(50) NOT NULL,
+	porte VARCHAR(50) NOT NULL,
     idpessoa INT NOT NULL,
     
-    INDEX idpessoa_idx (idpessoa),
-    
-    CONSTRAINT idpessoa_fk FOREIGN KEY (idpessoa) REFERENCES Pessoa(idpessoa) ON UPDATE RESTRICT ON DELETE RESTRICT
+    CONSTRAINT fk_idpessoa FOREIGN KEY (idpessoa) REFERENCES Pessoa(idpessoa) ON UPDATE RESTRICT ON DELETE RESTRICT
 );
 
-INSERT INTO pessoa VALUES (NULL, 'Lucas', '2004-08-16');
-INSERT INTO pet VALUES (NULL, 'Toto', 'Shitzu', '2020-09-18', 'Branco com preto', 1);
+CREATE INDEX idpessoa_idx ON Pessoa (idpessoa);
+
+INSERT INTO pessoa VALUES (default, 'Lucas', '2004-08-16');
+INSERT INTO pet VALUES (default, 'Toto', 'Shitzu', '2020-09-18', 'Branco com preto', 'Pequeno', 1);
 
 SELECT * FROM pessoa;
 SELECT * FROM pet;
